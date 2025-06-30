b9a66c155e094ea0e5a22daa830a23728014acb8
dmach1ne <danielahn010@gmail.com>
2025-06-30 23:09:19 +0900

    test.py

--- VecDB/test.py
+++ VecDB/test.py
@@ -5,6 +5,4 @@ import os
 load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
 
-tu.create_test_db(api_key=api_key,base_db_dir='./db_test',chunk_size=250,chunk_overlap=25)
-tu.create_test_db(api_key=api_key,base_db_dir='./db_test',chunk_size=500,chunk_overlap=50)
-tu.create_test_db(api_key=api_key,base_db_dir='./db_test',chunk_size=1000,chunk_overlap=100)
+tu.create_test_db(api_key=api_key,base_db_dir='./db_test')
